a8b11257e2ca7832735bba5c6a44ed28ce7551bf
Luke Skywalker <skywalker.eth@protonmail.com>
2022-08-09 19:02:55 +0530

    Farms fetched API integrated

---
+++ src/utils/api.ts
@@ -0,0 +1,49 @@
+import { createClient, defaultExchanges, gql } from "@urql/core";
+
+const client = createClient({
+  url: process.env.NEXT_PUBLIC_API_URL as string,
+  exchanges: defaultExchanges,
+});
+
+export const fetchListicleFarms = async () => {
+  const farmObj = await client
+    .query(
+      gql`
+        query Farms {
+          farms {
+            id
+            chef
+            chain
+            protocol
+            farmType
+            farmImpl
+            asset {
+              symbol
+              address
+              price
+              logos
+            }
+            tvl
+            rewards {
+              amount
+              asset
+              valueUSD
+              freq
+            }
+            apr {
+              reward
+              base
+            }
+            allocPoint
+          }
+        }
+      `
+    )
+    .toPromise();
+
+  const farms = farmObj?.data?.farms;
+
+  return {
+    farms,
+  };
+};
