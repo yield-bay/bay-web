b65e12f1163dd9a1e8182dbb05a89e0e02df05e2
Luke Skywalker <skywalker.eth@protonmail.com>
2022-08-24 20:43:51 +0530

    Button and Badge Update

--- src/components/Library/Button.tsx
+++ src/components/Library/Button.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 type ButtonProps = {
   children: React.ReactNode;
-  type: "primary" | "secondary";
+  type: "primary" | "secondary" | "tertiary";
   size: "small" | "large";
   onButtonClick?: () => void;
 };
@@ -15,12 +15,14 @@ export default function Button({
 }: ButtonProps) {
   return (
     <button
-      className={`flex flex-row items-center group sm:gap-x-1 font-semibold text-sm rounded-lg leading-[17px] transition duration-200 ${
+      className={`flex flex-row items-center group sm:gap-x-1 font-semibold text-base rounded-lg leading-[17px] transition duration-200 ${
         size == "small" ? "py-3 px-6" : "py-4 px-8"
       } ${
         type == "primary"
-          ? "bg-primaryWhite dark:bg-primaryBlue text-primaryBlue dark:text-primaryWhite"
-          : "bg-primaryBlue dark:bg-baseBlueMid text-primaryWhite dark:text-primaryWhite"
+          ? "bg-primaryWhite dark:bg-primaryBlue text-primaryBlue dark:text-white"
+          : type == "secondary"
+          ? "bg-primaryBlue dark:bg-blueSilver text-primaryWhite dark:text-baseBlueDark"
+          : "bg-bodyGray dark:bg-baseBlueMid text-primaryBlue dark:text-blueSilver"
       } `}
       onClick={onButtonClick}
     >
--- src/components/Library/FarmBadge.tsx
+++ src/components/Library/FarmBadge.tsx
@@ -1,9 +1,8 @@
 type FarmBadgeProps = {
-  children: string;
   type: "standard" | "stable" | "single";
 };
 
-export default function FarmBadge({ children, type }: FarmBadgeProps) {
+export default function FarmBadge({ type }: FarmBadgeProps) {
   return (
     <div className="flex px-3 py-1 bg-bodyGray rounded-[4px] w-max dark:bg-[#001942]">
       <span className="font-bold font-montserrat text-[10px] tracking-[0.36em] text-primaryBlue dark:text-white uppercase">
