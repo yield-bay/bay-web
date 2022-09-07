ea94129df5776a1a964422a21d7137595fbb8f02
Luke Skywalker <skywalker.eth@protonmail.com>
2022-09-07 14:12:47 +0530

    Scrolltotop btn position mobile

--- src/components/Library/ScrollToTopBtn.tsx
+++ src/components/Library/ScrollToTopBtn.tsx
@@ -3,7 +3,7 @@ import { ArrowUpIcon } from "@heroicons/react/solid";
 export default function ScrollToTopBtn() {
   return (
     <button
-      className="fixed bottom-[80px] right-[120px] z-20 p-[10px] rounded-full hover:scale-105 active:scale-100 bg-bodyGray dark:bg-primaryBlue transition-all ease-in-out duration-200"
+      className="fixed bottom-20 sm:bottom-[80px] right-12 sm:right-[120px] z-20 p-[10px] rounded-full hover:scale-105 active:scale-100 bg-bodyGray dark:bg-primaryBlue transition-all ease-in-out duration-200"
       onClick={() => {
         if (typeof window !== undefined) {
           window.scrollTo({
