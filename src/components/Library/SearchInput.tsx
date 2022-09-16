b0b98c7ea2e6f5f1abfdf8ca0ce3f006a8522b91
Luke Skywalker <skywalker.eth@protonmail.com>
2022-09-17 04:42:31 +0530

    feat/ esc key to unFocus search bar

--- src/components/Library/SearchInput.tsx
+++ src/components/Library/SearchInput.tsx
@@ -18,6 +18,12 @@ export default function SearchInput({ term, setTerm }: SearchInputProps) {
     }
   });
 
+  useKeyPress(["Escape"], (event: any) => {
+    if (ref.current != null) {
+      ref.current.blur();
+    }
+  });
+
   return (
     <div className="relative flex w-full max-w-sm sm:max-w-md lg:max-w-[482px] text-primaryBlue dark:text-primaryWhite rounded-md shadow-sm ring-transparent">
       <div className="absolute pl-4 sm:pl-6 lg:pl-9 left-0 inset-y-0 flex items-center pointer-events-none">
@@ -40,13 +46,15 @@ export default function SearchInput({ term, setTerm }: SearchInputProps) {
         className="block w-full pl-12 pr-4 lg:pl-[84px] py-3 focus:ring-[3px] ring-[#8EB8FF] dark:ring-baseBlueMid placeholder:text-primaryBlue dark:placeholder:text-primaryWhite text-xs sm:text-base leading-4 sm:leading-5 font-semibold bg-bodyGray dark:bg-baseBlueDark border-none outline-none rounded-xl transition duration-200"
         placeholder="Search by token, chain or protocol name"
       />
-      {!inputFocus && (
-        <div className="absolute hidden md:flex items-center pr-9 right-0 inset-y-0 pointer-events-none">
-          <div className="flex px-[9px] py-[3.5px] max-w-max border-[0.5px] border-primaryBlue dark:border-primaryWhite rounded-[3px] transition duration-200">
-            <span className="text-xs font-medium leading-[14.5px]">/</span>
-          </div>
+      {/* {!inputFocus && ( */}
+      <div className="absolute hidden md:flex items-center pr-9 right-0 inset-y-0 pointer-events-none">
+        <div className="flex px-[9px] py-[3.5px] max-w-max border-[0.5px] border-primaryBlue dark:border-primaryWhite rounded-[3px] transition duration-200">
+          <span className="text-xs font-medium leading-[14.5px]">
+            {!inputFocus ? "/" : "esc"}
+          </span>
         </div>
-      )}
+      </div>
+      {/* )} */}
     </div>
   );
 }
