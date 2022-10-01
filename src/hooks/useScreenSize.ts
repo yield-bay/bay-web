7361828e583f2241999a1116a195c00e80281c3c
Luke Skywalker <skywalker.eth@protonmail.com>
2022-10-01 22:01:08 +0530

    :zap: useScreensize hook w/ debounce util to fix dual rendering

---
+++ src/hooks/useScreenSize.ts
@@ -0,0 +1,50 @@
+/*
+  –– useScreenSize Hook ––
+  This hook uses "resize" event handler to return the responsive domain of the screensize
+  based on the predefined values in tailwindcss.
+  It uses a debounce func. to not fire screensize continuously.
+*/
+
+import { useState, useEffect } from "react";
+import debounce from "@utils/debounce";
+
+const calculateScreenWidth = (windowWidth: number) => {
+  if (windowWidth >= 1536) {
+    return "2xl";
+  } else if (windowWidth >= 1280) {
+    return "xl";
+  } else if (windowWidth >= 1024) {
+    return "lg";
+  } else if (windowWidth >= 768) {
+    return "md";
+  } else if (windowWidth >= 640) {
+    return "sm";
+  } else {
+    return "xs";
+  }
+};
+
+const useScreenSize = () => {
+  const [windowWidth, setWindowWidth] = useState(0);
+  const [responsiveWidth, setResponsiveWidth] = useState("xs");
+
+  useEffect(() => {
+    const handleResize = debounce(() => {
+      setWindowWidth(window.innerWidth);
+    }, 500);
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    setResponsiveWidth(
+      calculateScreenWidth(windowWidth == 0 ? window.innerWidth : windowWidth)
+    );
+  }, [windowWidth]);
+
+  return responsiveWidth;
+};
+
+export default useScreenSize;
