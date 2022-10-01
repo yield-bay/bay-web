5ca9d7fc51704f9259da1576c4d89e30b7e4f5aa
Luke Skywalker <skywalker.eth@protonmail.com>
2022-10-01 22:38:52 +0530

    :boom: Revamped MetaTags completly. Now modular

---
+++ src/metaTags/config.ts
@@ -0,0 +1,11 @@
+const config = {
+  siteName: "list.yieldbay.io",
+  defaultTitle: "YieldBay List",
+  defaultDescription:
+    "Discover yield opportunities across multiple protocols and parachains on Polkadot and Kusama.",
+  defaultDomain: "https://list.yieldbay.io",
+  defaultImage: `https://list.yieldbay.io/twitter-cover.png`, // OG like twitter card generally needs full path of image, not relative
+  defaultUsername: "@yield_bay",
+};
+
+export default config;
