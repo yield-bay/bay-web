0ac0f6599f37f85194dba52a1ee9c8dabcd53ffc
chilpatax <chilpatax@protonmail.com>
2022-11-25 19:54:47 +0530

    Center Align Rewards ToolTip

--- src/components/Library/Rewards.tsx
+++ src/components/Library/Rewards.tsx
@@ -17,7 +17,10 @@ export default function Rewards({ rewards }: RewardsProps) {
         content={
           <div>
             {rewards.map((reward, index) => (
-              <div key={index} className="flex justify-start gap-x-2">
+              <div
+                key={index}
+                className="flex justify-start items-center gap-x-2"
+              >
                 <div>
                   <Image
                     src={`https://raw.githubusercontent.com/yield-bay/assets/main/list/${reward.asset}.png`}
