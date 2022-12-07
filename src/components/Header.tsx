312a9fea63cdc268bb9dbae0a20f857bcdfcf68d
Arrow <117679760+Arrow-YB@users.noreply.github.com>
2022-12-07 22:54:53 +0530

    added working wallet modal

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 import Button from "./Library/Button";
 import { useAccount, useConnect, useDisconnect } from "wagmi";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { hashAtom } from "@store/atoms";
 import { useAtom } from "jotai";
-import Modal from "./Modal";
+import { Dialog, Transition } from "@headlessui/react";
 
 async function fetchUserHash(address: `0x${string}` | undefined) {
   const query = { address };
@@ -17,6 +17,71 @@ async function fetchUserHash(address: `0x${string}` | undefined) {
   ).json();
   console.log(data.hash);
   return data.hash;
+}
+
+function Modal() {
+  let [isOpen, setIsOpen] = useState(false);
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  return (
+    <>
+      <div className="items-center justify-center">
+        <Button onButtonClick={openModal} size="small">
+          Connect Wallet
+        </Button>
+      </div>
+
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-[#010C1D] p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-white"
+                  >
+                    Select Wallet
+                  </Dialog.Title>
+
+                  <div className="mt-4 grid place-items-center space-y-7">
+                    <Profile />
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
 }
 
 function Profile() {
@@ -74,7 +139,6 @@ export default function Header() {
         >
           <Button size="small">List your protocol</Button>
         </a>
-        <Profile />
         <Modal />
       </div>
     </div>
