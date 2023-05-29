4e87cc7c689c7e3db5341c4d3ea5badc6342a563
Luke Skywalker <skywalker.eth@protonmail.com>
2023-05-30 03:58:02 +0530

    filtering options: chain, amount, search

--- src/hooks/useFilteredPositionType.ts
+++ src/hooks/useFilteredPositionType.ts
@@ -4,30 +4,23 @@
  * @param id - Id assigned to position types, ie. 1 = all, 2 = standard, 3 = stable, 4 = single
  * @returns - Filtered list of positions according to option select
  */
-export default function useFilteredByChain(
+export default function useFilteredPositionType(
   positions: any[],
   id: number
 ): any[] {
   if (!positions) return [];
   else if (id == 0) return positions; // All
   else if (id == 1) {
+    // Idle
     const filtered = positions.filter(
-      (position) => position?.chain == "moonriver"
+      (position) => position?.unstaked.amount > 0
     );
     return filtered;
   } else if (id == 2) {
+    // Staked
     const filtered = positions.filter(
-      (position) => position?.chain == "moonbeam"
+      (position) => position?.staked.amount > 0
     );
-    return filtered;
-  } else if (id == 3) {
-    const filtered = positions.filter((position) => position?.chain == "astar");
-    return filtered;
-  } else if (id == 4) {
-    const filtered = positions.filter(
-      (position) => position?.chain == "mangata"
-    );
-
     return filtered;
   }
   return positions;
