bad1408f2c9c7562616bd6c3dab5898c3315a4db
Luke Skywalker <skywalker.eth@protonmail.com>
2023-06-01 22:47:51 +0530

    Responsiveness Main page + Header

--- src/components/App/Hero.tsx
+++ src/components/App/Hero.tsx
@@ -1,34 +1,19 @@
 import { FC, memo } from "react";
 import FarmStats from "@components/Library/FarmStats";
-import SelectFarmType from "@components/Library/SelectFarmType";
 import { protocolCount, tvlCount } from "@utils/statsMethods";
-import { AdjustmentsIcon } from "@heroicons/react/outline";
-import SearchInput from "@components/Library/SearchInput";
 import { FarmType } from "@utils/types";
 
 interface Props {
-  setProtocolModalOpen: (value: boolean) => void;
   farms: FarmType[];
-  setPrefModalOpen: (value: boolean) => void;
-  filteredFarms: FarmType[];
-  searchTerm: string;
-  setSearchTerm: (value: string) => void;
 }
 
-const Hero: FC<Props> = ({
-  setProtocolModalOpen,
-  farms,
-  setPrefModalOpen,
-  filteredFarms,
-  searchTerm,
-  setSearchTerm,
-}) => {
+const Hero: FC<Props> = ({ farms }) => {
   return (
-    <div className="bg-hero-gradient-mob sm:bg-hero-gradient">
+    <div className="px-6 sm:px-0">
       {/* Center Container */}
-      <div className="mx-auto max-w-lg md:max-w-xl mb-10 mt-[13px]">
+      <div className="mx-auto max-w-[278px] md:max-w-xl mb-10 mt-4 sm:mt-[13px]">
         <h1
-          className="mb-11 sm:mb-4 font-satoshi font-bold text-2xl px-4 sm:text-3xl md:text-[32px] leading-[30.62px] sm:leading-10 md:leading-[38.4px] text-center text-[#D9D9D9]"
+          className="mb-11 sm:mb-4 sm:px-4 font-satoshi font-bold text-[28px] sm:text-3xl md:text-[32px] leading-8 sm:leading-10 md:leading-[38.4px] text-center text-[#D9D9D9]"
           id="hero-heading"
         >
           discover & earn yield from polkadot and kusama paraverse
@@ -41,33 +26,6 @@ const Hero: FC<Props> = ({
           />
         </div>
       </div>
-      {/* <div
-        className="flex flex-col-reverse sm:flex-row items-center justify-between border-y sm:border-none border-[#334380] border-opacity-60
-                          mt-8 sm:mt-0 py-0 sm:py-10 px-0 sm:px-6 md:pl-16 md:pr-8 lg:px-28 bg-hero-gradient-mob
-                          font-medium text-base text-blueSilver leading-5"
-      >
-        <div className="flex items-center py-5 sm:py-0 px-9 sm:px-0 justify-between w-full sm:w-max sm:gap-x-5">
-          <div className="hidden sm:block">
-            <SelectFarmType />
-          </div>
-          <div className="sm:hidden" onClick={() => setPrefModalOpen(true)}>
-            <AdjustmentsIcon className="w-4 h-4 rotate-90" />
-          </div>
-          <div className="sm:hidden min-w-max py-1 px-2">
-            {filteredFarms.length == 0
-              ? "Loading..."
-              : `${filteredFarms.length} Results`}
-          </div>
-        </div>
-        <div className="flex border-b sm:border-none border-[#334380] border-opacity-40 w-full justify-center sm:justify-end lg:justify-center">
-          <SearchInput term={searchTerm} setTerm={setSearchTerm} />
-        </div>
-        <div className="hidden sm:block min-w-max py-1 px-2 opacity-50">
-          {filteredFarms.length == 0
-            ? "Loading..."
-            : `${filteredFarms.length} Results`}
-        </div>
-      </div> */}
     </div>
   );
 };
--- src/components/Library/SearchInput.tsx
+++ src/components/Library/SearchInput.tsx
@@ -25,8 +25,8 @@ export default function SearchInput({ term, setTerm }: SearchInputProps) {
   });
 
   return (
-    <div className="relative border flex w-full max-w-xs xl:max-w-[437px] text-[#667085] sm:rounded-lg ring-transparent">
-      <div className="absolute pl-10 sm:pl-2 lg:pl-[10px] left-0 inset-y-0 flex items-center pointer-events-none">
+    <div className="relative border flex w-full max-w-xs xl:max-w-[437px] text-[#667085] rounded-lg ring-transparent">
+      <div className="absolute pl-[12px] sm:pl-2 lg:pl-[10px] left-0 inset-y-0 flex items-center pointer-events-none">
         <Image
           src="/icons/SearchIcon.svg"
           alt="search"
@@ -45,7 +45,7 @@ export default function SearchInput({ term, setTerm }: SearchInputProps) {
         ref={ref}
         onFocus={() => setInputFocus(true)}
         onBlur={() => setInputFocus(false)}
-        className="block w-full pl-20 sm:pl-9 lg:pl-[42px] pr-4 py-6 sm:py-[10px] sm:ring-[1px] focus:ring-[3px] ring-[#D0D5DD] placeholder:text-[#667085] text-sm sm:text-base leading-[17px] sm:leading-6 font-normal bg-transparent border-none outline-none sm:rounded-lg"
+        className="block w-full pl-10 sm:pl-9 lg:pl-[42px] pr-4 py-[10px] ring-[1px] focus:ring-[2px] ring-[#D0D5DD] placeholder:text-[#667085] text-base leading-6 font-normal bg-transparent border-none outline-none rounded-lg"
         placeholder="Search"
       />
       <div className="absolute hidden lg:flex items-center pr-[14px] right-0 inset-y-0 pointer-events-none">
