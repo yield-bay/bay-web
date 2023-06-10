40aa3ace4640402dfb4bf1669287319bf7251b6c
Jack Sparrow <capjacksparrow42@protonmail.com>
2023-06-10 15:54:31 +0530

    prod env

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,6 @@
 // STATE
-export const IS_PRODUCTION = process.env.NODE_ENV === "production";
+export const IS_PRODUCTION =
+  process.env.NEXT_PUBLIC_IS_PRODUCTION === "production";
 
 // API URLS
 export const API_URL = process.env.NEXT_PUBLIC_API_URL as string;
