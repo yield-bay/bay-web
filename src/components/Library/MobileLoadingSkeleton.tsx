8cffd50f4f5e25e94d2495c9b5b6f67706192699
Luke Skywalker <skywalker.eth@protonmail.com>
2023-06-16 01:54:24 +0530

    rewards tooltip / loading-skeleton theme

--- src/components/Library/MobileLoadingSkeleton.tsx
+++ src/components/Library/MobileLoadingSkeleton.tsx
@@ -5,7 +5,7 @@ export default function MobileLoadingSkeleton() {
       {lengthArray.map((ele) => (
         <div
           key={ele}
-          className="w-full p-9 border-b border-[#222A39] transition-all duration-200"
+          className="w-full p-9 border-b border-[#222A39] bg-white transition-all duration-200"
         >
           {/* Upper Container for left and right */}
           <div className="flex flex-row justify-between">
@@ -15,37 +15,37 @@ export default function MobileLoadingSkeleton() {
                 <div className="flex flex-row justify-start -gap-x-2">
                   {[1, 2, 3, 4].map((ele, index) => (
                     <div key={index}>
-                      <div className="z-10 flex overflow-hidden ring-2 ring-baseBlueDark rounded-full bg-neutral-800 transition duration-200 animate-shiny">
-                        <div className="w-9 h-9 rounded-full bg-baseBlueMid animate-shiny" />
+                      <div className="z-10 flex overflow-hidden ring-2 ring-gray-200 rounded-full bg-neutral-800 transition duration-200 animate-shiny">
+                        <div className="w-9 h-9 rounded-full bg-gray-300 animate-shiny" />
                       </div>
                     </div>
                   ))}
                 </div>
               </div>
               <div>
-                <div className="w-[122px] h-[17px] bg-baseBlueMid rounded-2xl animate-shiny" />
+                <div className="w-[122px] h-[17px] bg-gray-300 rounded-2xl animate-shiny" />
               </div>
               <div>
-                <div className="h-[17px] w-36 bg-baseBlueMid rounded-2xl animate-shiny" />
+                <div className="h-[17px] w-36 bg-gray-300 rounded-2xl animate-shiny" />
               </div>
-              <div className="w-32 h-3 bg-baseBlueMid rounded-[3px] animate-shiny" />
+              <div className="w-32 h-3 bg-gray-300 rounded-[3px] animate-shiny" />
             </div>
             {/* RIGHT */}
             <div className="flex flex-col gap-y-[18px] font-medium font-spaceGrotesk text-right">
               <div className="w-full flex justify-end">
-                <div className="h-[17px] w-[53px] bg-baseBlueMid rounded-2xl animate-shiny" />
+                <div className="h-[17px] w-[53px] bg-gray-300 rounded-2xl animate-shiny" />
               </div>
               <div className="w-full inline-flex justify-end items-center gap-x-2">
-                <div className="h-[17px] w-[53px] bg-baseBlueMid rounded-2xl animate-shiny" />
-                <div className="h-[17px] w-[17px] bg-baseBlueMid rounded-2xl animate-shiny"></div>
+                <div className="h-[17px] w-[53px] bg-gray-300 rounded-2xl animate-shiny" />
+                <div className="h-[17px] w-[17px] bg-gray-300 rounded-2xl animate-shiny"></div>
               </div>
             </div>
           </div>
           <div className="flex flex-row gap-x-3 justify-between mt-9">
             <div>
-              <div className="h-12 w-[54px] rounded-lg bg-baseBlueMid animate-shiny" />
+              <div className="h-12 w-[54px] rounded-lg bg-gray-300 animate-shiny" />
             </div>
-            <div className="h-12 w-[251px] bg-baseBlueMid rounded-lg animate-shiny" />
+            <div className="h-12 w-[251px] bg-gray-300 rounded-lg animate-shiny" />
           </div>
         </div>
       ))}
--- src/components/Library/Rewards.tsx
+++ src/components/Library/Rewards.tsx
@@ -15,20 +15,18 @@ export default function Rewards({ rewards }: RewardsProps) {
     <div className="flex justify-start w-full">
       <Tooltip
         label={
-          <div>
+          <div className="flex flex-col gap-2">
             {rewards.map((reward, index) => (
               <div
                 key={index}
                 className="flex justify-start items-center gap-x-2"
               >
-                <div>
-                  <Image
-                    src={`https://raw.githubusercontent.com/yield-bay/assets/main/list/${reward.asset}.png`}
-                    alt={reward.asset}
-                    width={12}
-                    height={12}
-                  />
-                </div>
+                <Image
+                  src={`https://raw.githubusercontent.com/yield-bay/assets/main/list/${reward.asset}.png`}
+                  alt={reward.asset}
+                  width={12}
+                  height={12}
+                />
                 <span>
                   {parseFloat(reward.amount.toFixed(1)).toLocaleString("en-US")}
                 </span>
@@ -39,6 +37,7 @@ export default function Rewards({ rewards }: RewardsProps) {
             ))}
           </div>
         }
+        placement="top"
       >
         <div className="inline-flex -space-x-1">
           {rewards.map((reward, index) => (
