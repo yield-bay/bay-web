6bc3c10d98e30b6d0bc89fc271c1ffd927473276
Jack Sparrow <94821659+capjacksparrow42@users.noreply.github.com>
2023-07-17 17:09:04 +0530

    curve abi

--- src/components/Common/Layout/evmUtils.tsx
+++ src/components/Common/Layout/evmUtils.tsx
@@ -246,7 +246,7 @@ export const curveLpAbi = [
   "function balanceOf(address) external view returns (uint256)",
   "function calc_token_amount(uint256[2], bool) external view returns (uint256)",
   "function calc_withdraw_one_coin(uint256, int128) external view returns (uint256)",
-  "function add_liquidity(uint256[], uint256) external returns (uint256)",
+  "function add_liquidity(uint256[2], uint256) external returns (uint256)",
   "function remove_liquidity(uint256, uint256[2]) external returns (uint256[])",
   "function remove_liquidity_one_coin(uint256, int128, uint256) external returns (uint256)",
 ];
