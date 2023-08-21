6f0394d8a38bd537acc8e9a2ef51a7b4f4950c88
Sahil <sahilnanda1995@gmail.com>
2023-08-21 19:00:11 +0530

    banner mobile view done

--- src/components/Common/Header/Banner.tsx
+++ src/components/Common/Header/Banner.tsx
@@ -9,24 +9,24 @@ const Banner = ({
   onClose: () => void;
 }) => {
   return (
-    <div className="mb-12">
-      <div className="bg-[#E0E0FF] text-[#080814] py-2 px-4 font-semibold fixed w-full top-0 left-0 z-50 flex justify-center items-center">
-        <div className="flex flex-row w-full justify-center items-center">
-          <span>
-            GM! If you like using yieldbay, we are up for vite on Polkassembly
-            to onboard the next 3B Users to Polkadot. Please Vote!
-          </span>
+    <div className="sm:mb-12 mb-44">
+      <div className="bg-[#E0E0FF] text-[#080814] py-6 sm:py-2 px-6 font-semibold fixed w-full top-0 z-50 flex sm:flex-row flex-col text-justify justify-center items-center">
+        <span>
+          GM! If you like using yieldbay, we are up for vite on Polkassembly to
+          onboard the next 3B Users to Polkadot. Please Vote!
+        </span>
+        <div className="sm:ml-6 flex flex-row justify-center items-center sm:mt-0 mt-4">
           <Link
             href="https://polkadot.subsquare.io/referenda/referendum/97"
             target="_blank"
-            className="ml-4 text-sm px-12 py-3 font-bold rounded-lg bg-[#5C5CFF] text-white"
+            className="text-sm px-12 py-3 font-bold rounded-lg bg-[#5C5CFF] text-white"
           >
             Vote
           </Link>
+          <button onClick={onClose} className="ml-12">
+            <XIcon height={24} width={24} />
+          </button>
         </div>
-        <button onClick={onClose} className="mr-12">
-          <XIcon height={24} width={24} />
-        </button>
       </div>
     </div>
   );
