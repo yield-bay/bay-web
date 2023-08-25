fe6c57833dbd359a14b3387916db9d91fd8158a1
Luke Skywalker <skywalker.eth@protonmail.com>
2023-08-26 03:54:18 +0530

    revamp: CTA Buttons

--- src/components/Library/Button.tsx
+++ src/components/Library/Button.tsx
@@ -1,6 +1,7 @@
 import clsx from "clsx";
 import { ReactNode, memo } from "react";
 import Tooltip from "./Tooltip";
+import { ButtonType } from "@utils/types/enums";
 
 type ButtonProps = {
   children: ReactNode;
@@ -9,6 +10,7 @@ type ButtonProps = {
   disabled?: boolean;
   onButtonClick?: () => void;
   tooltipText?: string;
+  type?: ButtonType;
 };
 
 const Button = ({
@@ -18,21 +20,27 @@ const Button = ({
   disabled,
   onButtonClick,
   tooltipText = "",
+  type = ButtonType.SECONDARY,
 }: ButtonProps) => {
   return (
     <Tooltip label={disabled && <span>{tooltipText}</span>} placement="top">
       <button
         className={clsx(
-          "ring-1 ring-[#D0D5DD] text-[#1D2939] font-medium text-sm leading-5 rounded-lg shadow-farmBtn",
-          "disabled:opacity-40",
+          "font-medium text-sm leading-5 rounded-lg shadow-farmBtn",
+          "disabled:opacity-50",
           style,
+          type == ButtonType.SECONDARY
+            ? "bg-[#F9FAFB] ring-1 ring-[#D0D5DD] text-[#1D2939]"
+            : type == ButtonType.PRIMARY
+            ? "bg-[#5C5CFF] ring-1 ring-[#5C5CFF] text-white"
+            : "",
           size == "large"
-            ? "py-[10px] px-4 bg-[#F9FAFB]"
+            ? "py-[10px] px-4"
             : size == "custom"
             ? "py-[10px] px-4"
             : size == "base"
-            ? "py-[10.5px] px-6 rounded-[8px] bg-[#F9FAFB]"
-            : "py-2 px-[18px] sm:py-3 sm:px-6 bg-[#F9FAFB]"
+            ? "py-[10.5px] px-6 rounded-[8px]"
+            : "py-2 px-[18px] sm:py-3 sm:px-6"
         )}
         disabled={disabled}
         onClick={onButtonClick}
